7e01b3a8d6f8f72203389b69056a77d14e0e3019
Wesley Vance <wslyvnce@gmail.com>
2023-01-23 20:48:24 -0600

    finished CRUD API calls

--- api/memberData.js
+++ api/memberData.js
@@ -33,4 +33,44 @@ const getSingleMember = (firebaseKey) => new Promise((resolve, reject) => {
     .catch((reject));
 });
 
-export { getMembers, getSingleMember };
+const createMember = (payload) => new Promise((resolve, reject) => {
+  fetch(`${dbUrl}/members.json`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const updateMember = (payload) => new Promise((resolve, reject) => {
+  fetch(`${dbUrl}/members.json/${payload.firebaseKey}.json`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const deleteSingleMember = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${dbUrl}/members/${firebaseKey}.json`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+export {
+  getMembers, getSingleMember, createMember, updateMember, deleteSingleMember,
+};
