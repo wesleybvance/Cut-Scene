5f6a4a8bd091950bd0dae67692b2f6fe096ebbc1
Wesley Vance <wslyvnce@gmail.com>
2023-01-24 09:05:14 -0600

    building code in return of MemberForm for form input rendering

---
+++ components/forms/MemberForm.js
@@ -0,0 +1,96 @@
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { FloatingLabel, Form } from 'react-bootstrap';
+import { createMember, updateMember } from '../../api/memberData';
+import { useAuth } from '../../utils/context/authContext';
+
+const initialStateMF = {
+  first_name: '',
+  last_name: '',
+  image: '',
+  role: '',
+};
+
+export default function MemberForm({ obj }) {
+  const [formInput, setFormInput] = useState(initialStateMF);
+  const router = useRouter();
+  const { user } = useAuth();
+
+  useEffect(() => {
+    if (obj.firebaseKey) setFormInput(obj);
+  }, [obj]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormInput((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (obj.firebaseKey) {
+      updateMember(formInput)
+        .then(() => router.push(`/member/${obj.firebaseKey}`));
+    } else {
+      const payload = { ...formInput, uid: user.uid };
+      createMember(payload).then(({ name }) => {
+        const patchPayloadFBK = { firebaseKey: name };
+        updateMember(patchPayloadFBK).then(() => {
+          router.push('/authors');
+        });
+      });
+    }
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <h2>{obj.firebaseKey ? 'Update' : 'Add New'} Team Member</h2>
+      <FloatingLabel
+        controlId="floatingInput1"
+        label="First Name"
+        className="mb-3"
+      >
+        <Form.Control
+          type="text"
+          placeholder="Enter Member's First Name"
+          name="first_name"
+          value={formInput.first_name}
+          onChange={handleChange}
+          required
+        />
+      </FloatingLabel>
+      <FloatingLabel
+        controlId="floatingInput1"
+        label="Last Name"
+        className="mb-3"
+      >
+        <Form.Control
+          type="text"
+          placeholder="Enter Member's Last Name"
+          name="first_name"
+          value={formInput.first_name}
+          onChange={handleChange}
+          required
+        />
+      </FloatingLabel>
+    </Form>
+  );
+}
+
+MemberForm.propTypes = {
+  obj: PropTypes.shape({
+    first_name: PropTypes.string,
+    last_name: PropTypes.string,
+    image: PropTypes.string,
+    role: PropTypes.string,
+    uid: PropTypes.string,
+    firebaseKey: PropTypes.string,
+  }),
+};
+
+MemberForm.defaultProps = {
+  obj: initialStateMF,
+};
