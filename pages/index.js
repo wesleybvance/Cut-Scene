42dfeb25a24ccd2acc4365e472c7ba9ca3c6b780
Wesley Vance <wslyvnce@gmail.com>
2023-01-24 00:38:35 -0600

    created Home() function to map MemberCard component over member data on homepage

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,22 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
+import { getMembers } from '../api/memberData';
 import { useAuth } from '../utils/context/authContext';
+import MemberCard from '../components/MemberCard';
 
 function Home() {
   const { user } = useAuth();
+
+  const [members, setMembers] = useState([]);
+
+  const getAllMembers = () => {
+    getMembers(user.uid).then(setMembers);
+  };
+
+  useEffect(() => {
+    getAllMembers();
+  }, []);
 
   return (
     <><Head><title>Welcome to your Team</title></Head>
@@ -17,6 +31,10 @@ function Home() {
       >
         <h1>Hello, {user.displayName}! </h1>
         <h3> Your Movie Cast </h3>
+        <div>{members.map((member) => (
+          <MemberCard key={member.firebaseKey} memberObj={member} onUpdate={getAllMembers} />
+        ))}
+        </div>
       </div>
     </>
   );
