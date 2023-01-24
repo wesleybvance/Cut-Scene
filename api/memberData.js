5472b4ed6e34f92386086aa7c252868aaf5d85b7
Wesley Vance <wslyvnce@gmail.com>
2023-01-23 20:37:33 -0600

    building member API calls

--- api/memberData.js
+++ api/memberData.js
@@ -4,7 +4,7 @@ import { clientCredentials } from '../utils/client';
 const dbUrl = clientCredentials.databaseURL;
 
 const getMembers = (uid) => new Promise((resolve, reject) => {
-  fetch(`${dbUrl}/members.json?orderBy="uid"&equalTo="${uid}`, {
+  fetch(`${dbUrl}/members.json?orderBy="uid"&equalTo="${uid}"`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
@@ -13,10 +13,24 @@ const getMembers = (uid) => new Promise((resolve, reject) => {
     .then((response) => response.json())
     .then((data) => {
       if (data) {
-        resolve.apply(Object.values(data));
+        resolve(Object.values(data));
       } else {
         resolve([]);
       }
     })
     .catch(reject);
 });
+
+const getSingleMember = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${dbUrl}/members/${firebaseKey}.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application.json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch((reject));
+});
+
+export { getMembers, getSingleMember };
