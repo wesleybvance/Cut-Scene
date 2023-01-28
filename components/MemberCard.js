eb1e8852169a1cf96955a5a9d14a00f9101374a0
Wesley Vance <wslyvnce@gmail.com>
2023-01-28 04:24:13 -0600

    accidentally did a little more styling... fonts for buttons and card titles

--- components/MemberCard.js
+++ components/MemberCard.js
@@ -16,16 +16,16 @@ function MemberCard({ memberObj, onUpdate }) {
         <Card.Img variant="top" src={memberObj.image} alt={`${memberObj.first_name} ${memberObj.last_name}`} />
         <Card.Body style={{ color: 'black' }}>
           <div className="member-card-align">
-            <Card.Title>{memberObj.first_name} {memberObj.last_name}</Card.Title>
+            <Card.Title><h4 className="card-title">{memberObj.first_name} {memberObj.last_name}</h4></Card.Title>
             <Card.Text>
               <Link href={`team/${memberObj.firebaseKey}`} passHref>
-                <Button variant="outline-dark" className="m-2">VIEW</Button>
+                <Button variant="outline-dark" className="m-2 card-button">view</Button>
               </Link>
               <Link href={`team/edit/${memberObj.firebaseKey}`} passHref>
-                <Button variant="outline-dark" className="m-2">EDIT</Button>
+                <Button variant="outline-dark" className="m-2 card-button">edit</Button>
               </Link>
             </Card.Text>
-            <Button variant="outline-danger" onClick={deleteMemberBtn}>DELETE</Button>
+            <Button className="m-2 card-button" variant="outline-danger" onClick={deleteMemberBtn}>delete</Button>
           </div>
         </Card.Body>
       </Card>
